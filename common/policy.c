86858922af702471987c334f3fb3e3b3e45e1509
Jakob Sailer <jakob.sailer@gmx.at>
2022-04-25 22:52:55 +0200

    add functions for manipulating tag bits in memory in common folder

---
+++ common/policy.c
@@ -0,0 +1,78 @@
+// DIFT Policy
+//  Functions to change the DIFT Propagation and Check Policies
+// Autor:   Jakob Sailer, BSc
+// created: 2021-04-25
+
+
+//
+// INCLUDES
+//
+
+#include "../common/policy.h"
+
+
+
+//
+// GLOBAL VARIABLES - IMPLEMENTATION
+//
+
+// standard propagation policy
+const dift_prop_pol_t prop_policy_default = { .S =
+    {
+      .floats   = 0b01,   // OR
+      .mul      = 0b01,   // OR
+      .csr      = 0b0,    // ZERO
+      .comp     = 0b00,   // ZERO
+      .shift    = 0b01,   // OR
+      .alu      = 0b01,   // OR
+      .load     = 0b1001, // val=1, addr=0, pol=OR
+      .store    = 0b1001, // val=1, addr=0, pol=OR
+      .reserved = 0
+    }
+  };
+
+// standard check policy
+const dift_check_pol_t check_policy_default = { .S =
+    {
+      .load     = 0b1,
+      .store    = 0b1,
+      .branch   = 0b000,  // mode=OFF, single_mode_select=OP_A
+      .jalr     = 0b1,
+      .exec     = 0b1,
+      .reserved = 0
+    }
+  };
+
+
+
+//
+// GLOBAL FUNCTIONS - IMPLEMENTATION
+//
+
+// change DIFT propagation policy
+void change_prop_policy(dift_prop_pol_t prop_pol)
+{
+  uint32_t value;
+  
+  value = prop_pol.U;
+  
+  // CSR 0x20 is DIFT TPCR
+  __asm__ volatile ( "csrw 0x20, %[value_reg]"
+                   :
+                   : [value_reg] "r" (value) );
+}
+
+
+// change DIFT check policy
+void change_check_policy(dift_check_pol_t check_pol)
+{
+  uint32_t value;
+  
+  value = check_pol.U;
+
+  // CSR 0x21 is DIFT TCCR
+  __asm__ volatile ( "csrw 0x21, %[value_reg]"
+                   :
+                   : [value_reg] "r" (value) );
+}
+
---
+++ common/policy.h
@@ -0,0 +1,73 @@
+// DIFT Policy
+//  Functions to change the DIFT Propagation and Check Policies
+// Autor:   Jakob Sailer, BSc
+// created: 2021-04-25
+
+#ifndef INCLUDE_POLICY_H_
+#define INCLUDE_POLICY_H_
+
+//
+// INCLUDES
+//
+
+#include <rt/rt_api.h>
+
+
+
+//
+// TYPE DEFINITIONS
+//
+
+// tag propagation policy
+typedef union
+{
+  uint32_t U;
+  struct
+  {
+    uint32_t floats   : 2;
+    uint32_t mul      : 2;
+    uint32_t csr      : 1;
+    uint32_t comp     : 2;
+    uint32_t shift    : 2;
+    uint32_t alu      : 2;
+    uint32_t load     : 4;
+    uint32_t store    : 4;
+    uint32_t reserved : 13;
+  } S;
+} dift_prop_pol_t;
+
+// tag check policy
+typedef union
+{
+  uint32_t U;
+  struct
+  {
+    uint32_t load     : 1;
+    uint32_t store    : 1;
+    uint32_t branch   : 3;
+    uint32_t jalr     : 1;
+    uint32_t exec     : 1;
+    uint32_t reserved : 25;
+  } S;
+} dift_check_pol_t;
+
+
+
+//
+// GLOBAL VARIABLE PROTOTYPES
+//
+extern const dift_prop_pol_t prop_policy_default;
+extern const dift_check_pol_t check_policy_default;
+
+
+
+//
+// FUNCTION PROTOTYPES
+//
+
+void change_prop_policy(dift_prop_pol_t prop_pol);
+void change_check_policy(dift_check_pol_t check_pol);
+
+
+
+#endif // INCLUDE_POLICY_H_
